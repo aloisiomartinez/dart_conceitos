cbd5a11ed3aa90eb3ff69aea244aa77c5f9b2b2f
Aloisio Martinez <aloisiojacobs36@gmail.com>
2022-02-09 17:39:51 -0300

    condifionais

--- lib/3_operadores/1_condicionais.dart
+++ lib/3_operadores/1_condicionais.dart
@@ -1,3 +1,11 @@
 void  main() {
-  
+  final idade = 18;
+
+  if(idade > 18) {
+    print("Você pode dirigir!");
+  } else if(idade == 18) {
+    print("Você pode dirigir!");
+  } else {
+    print("Você não pode dirigir!");
+  }
 }
