a4f98a264c5ebb570c638e7da140cbcc616bddab
Aloisio Martinez <aloisiojacobs36@gmail.com>
2022-02-11 14:17:32 -0300

    loops iterable

---
+++ lib/6_loops/1_iterables.dart
@@ -0,0 +1,8 @@
+void main() {
+  var numeros = List.generate(10, (10) => 10);
+
+  numeros.where((numero) => numero != 5)
+  .forEach((numero) => print(numero));
+
+  final numerosRemoverAte5 = numeros.skipWhile((numero) => numero <6).toList();
+}
