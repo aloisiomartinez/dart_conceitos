1e74ea03048b62d35ba3887821477b764079d62a
Aloisio Martinez <aloisiojacobs36@gmail.com>
2022-02-14 14:50:17 -0300

    sets

--- lib/funcoes_colecoes/3_sets.dart
+++ lib/funcoes_colecoes/3_sets.dart
@@ -16,5 +16,15 @@ void main() {
   numerosSet.add(2);
   numerosSet.add(1);
   print(numerosSet);
+
+  numerosLista.toSet();
+
+  var numeros1 = {1,2,3,4,5,6};
+  var numeros2 = {1,2,3,4,5,6,7};
+
+  print(numeros1.difference(numeros2));
+  print(numeros1.union(numeros2));
+  print(numeros1.intersection(numeros2));
+
 }
 
