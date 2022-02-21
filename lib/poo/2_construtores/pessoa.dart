ba05ef095f3d5f80227d8079c696011278b747b7
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-02-21 10:04:34 -0300

    constructors

--- lib/poo/2_construtores/pessoa.dart
+++ lib/poo/2_construtores/pessoa.dart
@@ -11,9 +11,19 @@ class Pessoa {
     //sexo = sexoConstruct;
   //}
 
+  Pessoa.vazia();
+
   Pessoa({
     required this.nome,
     required this.idade,
     required this.sexo
   });
+
+  //Factory
+  factory Pessoa.fabrica(String nomeConstr) {
+    var nome = nomeConstr + 'Teste Factory';
+    var pessoa = Pessoa.vazia();
+    pessoa.nome = nome;
+    return pessoa;
+  }
 }
