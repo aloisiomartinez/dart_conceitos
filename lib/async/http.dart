287153e8a3043ca6485a83be17de99f7e2e14627
Aloisio Martinez <aloisiojacobs36@gmail.com>
2022-03-07 20:23:50 -0300

    http dart

---
+++ lib/async/http.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+Future<void> main() async {
+  var url = 'https://viacep.com.br/ws/01001000/json/';
+
+  var response = await http.get(Uri.parse(url));
+
+  if(response.statusCode == 200) {
+    var responseData = convert.jsonDecode(response.body);
+    print(responseData);
+  } else {
+    print('Erro ao fazer requisição');
+  }
+}
