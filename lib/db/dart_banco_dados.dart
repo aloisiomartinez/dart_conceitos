de71ae1a983220c9c1bae724e1274d72beb70458
Aloisio Martinez <aloisiojacobs36@gmail.com>
2022-03-11 07:46:13 -0300

    insert and select

--- lib/db/dart_banco_dados.dart
+++ lib/db/dart_banco_dados.dart
@@ -1,3 +1,22 @@
-void main() {
+import 'package:dart_fundamentos/db/database.dart';
+
+void main() async {
+  final database = Database();
+  var mysqlConnection = await database.openConnection();
+
+  var resultado = await mysqlConnection.query(
+    'insert into aluno(id, nome) values(?,?)', 
+    [
+      null,
+      'Aloisio Martinez'
+    ],
+  );
+  print(resultado.affectedRows);
+  var alunos = await mysqlConnection.query('select * from aluno');
+  alunos.forEach((row) { 
+    print(row['id']);
+    print(row['nome']);
+   });
+
   
 }
